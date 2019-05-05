21542546a11c0b094218fc6d5ed29a98895f2ede
jkarlin929 <osida11572@gmail.com>
2019-05-05 19:32:48 -0400

    add count and time

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function CounterHooks() {
+  const [count, setCount] = useState(0);
+  const [time, setTime] = useState(new Date());
+
+  const handleClick = () => {
+    setCount(count + 1);
+    setTime(new Date());
+  }
+
   return (
     <div>
-      <h3 className="center">Welcome to the Counter of Life</h3>
+      <h3
+      className="center">
+        Welcome to the Counter of Life
+      </h3>
+      <button
+        className="center-block"
+        onClick={handleClick}>
+          {count}
+        </button>
+        <p className="center">
+          at: {`${time.getHours()}:${time.getMinutes()} : ${time.getSeconds()}`}
+        </p>
     </div>
   );
 }
